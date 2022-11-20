c24c24c79333b90f742022e177a5e1ae2d635e31
Victor Zhu <victorvctr.zh@gmail.com>
2022-11-19 16:23:38 -0800

    :sparkles: Add basic Table drawing for grid Moved from drawing borders to drawing table

--- Game.hs
+++ Game.hs
@@ -1,10 +1,13 @@
 -- UI, event handlers 
+{-# LANGUAGE OverloadedStrings #-}
 
 module Game where 
 
 import Data.Matrix 
+import Data.Text (pack)
 
 import Brick ( App(..), BrickEvent(..), EventM, Widget, (<+>), str, withBorderStyle, emptyWidget)
+import Brick.Widgets.Table
 import Brick.Widgets.Center (center)
 import Brick.Widgets.Border.Style (unicode)
 
@@ -30,29 +33,50 @@ drawRow =
     where
         row = TODO
 
-drawGrid :: Environment -> Widget Name
-drawGrid grid = 
-    withBorderStyle unicode $
-    borderWithLabel (str "Raccoon Rush") $
-    center (str "DATA_MATRIX HERE")
 
-drawCell :: Cell -> Widget Name
-drawCell Player =
-    withBorderStyle unicode $
-    border $
-    center (str "Player")
-drawCell Empty =
-    withBorderStyle unicode $
-    border $
-    center (str "")
-drawCell Trash =
-    withBorderStyle unicode $
-    border $
-    center (str "Trash")
-drawCell Wall =
-    withBorderStyle unicode $
-    border $
-    center (str "Wall")
+
+
+-- drawGrid :: Environment -> Widget Name
+drawGrid grid = setDefaultRowAlignment AlignMiddle $
+    setDefaultColAlignment AlignCenter $
+    convertMap2Table grid
+
+
+cell2string Player = "P"
+cell2string Empty = " "
+cell2string Trash = "T"
+cell2string Wall = "W"
+
+convertMap2Table :: Matrix Cell -> Table n
+convertMap2Table m = table (map (map (\x -> txt (pack (cell2string x)))) $ (toLists m))
+
+-- test table
+-- convertMap2Table :: Matrix String -> Table n
+-- convertMap2Table m = table (map (map (\x -> txt (pack x))) $ (toLists m))
+
+
+-- drawGrid grid = 
+--     withBorderStyle unicode $
+--     borderWithLabel (str "Raccoon Rush") $
+--     center (str "DATA_MATRIX HERE")
+
+-- drawCell :: Cell -> Widget Name
+-- drawCell Player =
+--     withBorderStyle unicode $
+--     border $
+--     center (str "Player")
+-- drawCell Empty =
+--     withBorderStyle unicode $
+--     border $
+--     center (str "")
+-- drawCell Trash =
+--     withBorderStyle unicode $
+--     border $
+--     center (str "Trash")
+-- drawCell Wall =
+--     withBorderStyle unicode $
+--     border $
+--     center (str "Wall")
 
 
 sampleLevel :: Environment 
