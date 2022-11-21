8959a06b4d052fe625fbb506dfa3c67f3cff0d3a
and-le <a1le@ucsd.edu>
2022-11-21 14:03:59 -0800

    Added basic movement logic

--- src/Sokoban.hs
+++ src/Sokoban.hs
@@ -11,6 +11,7 @@ module Sokoban
 import Data.Matrix
 import Data.List
 import Data.Vector 
+import Data.Ix (inRange)
 
 
 -- An object that the player can interact with in the game
@@ -44,47 +45,100 @@ wallCell = MkCell {gameObject=Wall, background=EmptyBG}
 trashcanCell :: Cell -- starting point of the trash
 trashcanCell = MkCell {gameObject=Trash, background=Trashcan}
 
+trashCell :: Cell -- a cell containing trash that is not the starting point
+trashCell = MkCell {gameObject=Trash, background=EmptyBG}
+
 emptystashCell :: Cell -- an empty stash
 emptystashCell = MkCell {gameObject=Empty, background=Stash}
 
 -- level definitions
 sampleLevel :: Environment 
 sampleLevel = fromLists [[emptyCell , emptyCell     , wallCell       , emptyCell]
-                        ,[emptyCell , emptyCell   , playerCell      , wallCell]
-                        ,[emptyCell, trashcanCell , emptystashCell , wallCell]
+                        ,[emptyCell , trashCell   , trashCell      , wallCell]
+                        ,[emptyCell, trashCell , playerCell , wallCell]
                         ,[wallCell , wallCell     , wallCell       , emptyCell]
                         ]
 
-data PlayerMovement = Up | Right | Down | Left 
+-- The types of valid moves
+data Movement = UpMv | RightMv | DownMv | LeftMv 
     deriving (Eq, Show)
 
--- Returns True if the given movement is valid given the current environment; False otherwise.
-isValidMove :: PlayerMovement -> Environment -> Bool 
-isValidMove pm env = isValidMoveCached pm env (getPlayerLocation environment)
+-- Represents a (row, column) index in an Environment
+type Location = (Int, Int)
 
--- Same as `isValidMove` but uses a cached player location for efficiency.
--- Ideally we will be storing the player's current location throughout the game so
--- we won't need to query for it upon each move.
-isValidMoveCached :: PlayerMovement -> Environment -> (Int, Int) -> Bool 
-isValidMoveCached pm env (row, col) = error "TODO"
+-- Returns the Cell at the given location in the environment.
+getCellAtLocation :: Location -> Environment -> Cell 
+getCellAtLocation (row, col) env = getElem row col env 
 
--- Add the rest of the logic here
+-- Returns True if the given movement with respect to the given location is valid for the 
+-- current environment; False otherwise.
+isValidMove :: Movement -> Location -> Environment -> Bool 
+isValidMove mv loc env = 
+    (isWithinEnvironment loc env) && 
+    (isWithinEnvironment destLoc env) && 
+    (not (containsWall destCell)) && 
+    (not (containsPlayer destCell)) && 
+    ((containsEmptySpace destCell) || (containsTrash destCell && isValidPush mv destLoc env))
+    where 
+        destLoc = getLocationAfterMovement mv loc  
+        destCell = getCellAtLocation destLoc env
+        nextDestLoc = getLocationAfterMovement mv destLoc 
+        nextDestCell = getCellAtLocation nextDestLoc
 
+-- Returns True if the pushing behavior from the given location in the given direction of movement
+-- is valid; False otherwise. Currently non-recursive.
+isValidPush :: Movement -> Location -> Environment -> Bool 
+isValidPush mv loc env = 
+    (isWithinEnvironment loc env) && 
+    (isWithinEnvironment destLoc env) && 
+    (containsEmptySpace destCell)
+    where 
+        destLoc = getLocationAfterMovement mv loc  
+        destCell = getCellAtLocation destLoc env
+
+-- Returns True if the given location is within the environment; False otherwise.
+isWithinEnvironment :: Location -> Environment -> Bool 
+isWithinEnvironment (row, col) env = (inRange rowRange row) && (inRange colRange col)
+    where 
+        rowRange = (1, getNumRows env)
+        colRange = (1, getNumCols env)
+
+-- Returns the Location that would result if the given Movement is applied to the current location.
+getLocationAfterMovement :: Movement -> Location -> Location 
+getLocationAfterMovement UpMv (row, col) = (row - 1, col)
+getLocationAfterMovement RightMv (row, col) = (row, col + 1)
+getLocationAfterMovement DownMv (row, col) = (row + 1, col)
+getLocationAfterMovement LeftMv (row, col) = (row, col - 1)
+
+-- Returns True if the given Cell contains a Wall, False otherwise.
+containsWall :: Cell -> Bool 
+containsWall (MkCell Wall _) = True 
+containsWall _ = False
+
+-- Returns True if the given Cell contains a Player; False otherwise.
+containsPlayer :: Cell -> Bool 
+containsPlayer (MkCell Player _) = True 
+containsPlayer _ = False
+
+-- Returns True if the given Cell contains an Empty space; False otherwise.
+containsEmptySpace :: Cell -> Bool 
+containsEmptySpace (MkCell Empty _) = True 
+containsEmptySpace _ = False
+
+-- Returns True if the given Cell contains a Trash; False otherwise.
+containsTrash :: Cell -> Bool 
+containsTrash (MkCell Trash _) = True 
+containsTrash _ = False
 
 -- Returns the (row, column) 1-based index of the player element in the given environment;
 -- if the player is not present, returns (-1, -1).
-getPlayerLocation :: Environment -> (Int, Int)
+getPlayerLocation :: Environment -> Location
 getPlayerLocation env = case helper of 
     Just location -> location 
     Nothing -> (-1, -1)
     where 
-        helper :: Maybe (Int, Int)
+        helper :: Maybe Location
         helper = findIndexMatrix env containsPlayer
-
--- Returns True if the given Cell contains the Player, False otherwise.
-containsPlayer :: Cell -> Bool 
-containsPlayer (MkCell Player _) = True 
-containsPlayer _ = False
 
 -- Returns the number of rows for the given rectangular matrix.
 getNumRows :: Matrix a -> Int 
@@ -96,10 +150,10 @@ getNumCols mat = Data.Vector.length (getCol 1 mat)
 
 -- Returns the first-occuring index of an element within the matrix that satisfies the given predicate;
 -- Nothing if the element was not found.
-findIndexMatrix :: (Eq a) => Matrix a -> (a -> Bool) -> Maybe (Int, Int)
+findIndexMatrix :: (Eq a) => Matrix a -> (a -> Bool) -> Maybe Location
 findIndexMatrix mat predicate = helper mat predicate (getNumRows mat) (getNumCols mat) 1 1
     where 
-    helper :: (Eq a) => Matrix a -> (a -> Bool) -> Int -> Int -> Int -> Int -> Maybe (Int, Int)
+    helper :: (Eq a) => Matrix a -> (a -> Bool) -> Int -> Int -> Int -> Int -> Maybe Location
     helper mat predicate numRows numCols row col
         | row > numRows = Nothing -- reached end of matrix without finding desired element
         | col > numCols = helper mat predicate numRows numCols (row + 1) 1 -- reached end of row
