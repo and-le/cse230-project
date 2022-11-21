7c898a995ebc6aee025e5aa7c44e187eaf999ee9
and-le <a1le@ucsd.edu>
2022-11-21 12:55:04 -0800

    Player location lookup

--- src/Sokoban.hs
+++ src/Sokoban.hs
@@ -9,6 +9,8 @@ module Sokoban
 )
     where
 import Data.Matrix
+import Data.List
+import Data.Vector 
 
 
 -- An object that the player can interact with in the game
@@ -47,10 +49,59 @@ emptystashCell = MkCell {gameObject=Empty, background=Stash}
 
 -- level definitions
 sampleLevel :: Environment 
-sampleLevel = fromLists [[wallCell , wallCell     , wallCell       , wallCell]
-                        ,[wallCell , playerCell   , emptyCell      , wallCell]
-                        ,[wallCell , trashcanCell , emptystashCell , wallCell]
-                        ,[wallCell , wallCell     , wallCell       , wallCell]
+sampleLevel = fromLists [[emptyCell , emptyCell     , wallCell       , emptyCell]
+                        ,[emptyCell , emptyCell   , playerCell      , wallCell]
+                        ,[emptyCell, trashcanCell , emptystashCell , wallCell]
+                        ,[wallCell , wallCell     , wallCell       , emptyCell]
                         ]
 
--- Add game logic functions here
+data PlayerMovement = Up | Right | Down | Left 
+    deriving (Eq, Show)
+
+-- Returns True if the given movement is valid given the current environment; False otherwise.
+isValidMove :: PlayerMovement -> Environment -> Bool 
+isValidMove pm env = isValidMoveCached pm env (getPlayerLocation environment)
+
+-- Same as `isValidMove` but uses a cached player location for efficiency.
+-- Ideally we will be storing the player's current location throughout the game so
+-- we won't need to query for it upon each move.
+isValidMoveCached :: PlayerMovement -> Environment -> (Int, Int) -> Bool 
+isValidMoveCached pm env (row, col) = error "TODO"
+
+-- Add the rest of the logic here
+
+
+-- Returns the (row, column) 1-based index of the player element in the given environment;
+-- if the player is not present, returns (-1, -1).
+getPlayerLocation :: Environment -> (Int, Int)
+getPlayerLocation env = case helper of 
+    Just location -> location 
+    Nothing -> (-1, -1)
+    where 
+        helper :: Maybe (Int, Int)
+        helper = findIndexMatrix env containsPlayer
+
+-- Returns True if the given Cell contains the Player, False otherwise.
+containsPlayer :: Cell -> Bool 
+containsPlayer (MkCell Player _) = True 
+containsPlayer _ = False
+
+-- Returns the number of rows for the given rectangular matrix.
+getNumRows :: Matrix a -> Int 
+getNumRows mat = Data.Vector.length (getRow 1 mat)
+
+-- Returns the number of columns for the given rectangular matrix.
+getNumCols :: Matrix a -> Int 
+getNumCols mat = Data.Vector.length (getCol 1 mat)
+
+-- Returns the first-occuring index of an element within the matrix that satisfies the given predicate;
+-- Nothing if the element was not found.
+findIndexMatrix :: (Eq a) => Matrix a -> (a -> Bool) -> Maybe (Int, Int)
+findIndexMatrix mat predicate = helper mat predicate (getNumRows mat) (getNumCols mat) 1 1
+    where 
+    helper :: (Eq a) => Matrix a -> (a -> Bool) -> Int -> Int -> Int -> Int -> Maybe (Int, Int)
+    helper mat predicate numRows numCols row col
+        | row > numRows = Nothing -- reached end of matrix without finding desired element
+        | col > numCols = helper mat predicate numRows numCols (row + 1) 1 -- reached end of row
+        | predicate (getElem row col mat) = Just (row, col)
+        | otherwise = helper mat predicate numRows numCols row (col + 1)
